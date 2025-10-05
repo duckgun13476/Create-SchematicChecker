8eaaf80210b6c56b58357c161f9b23f73b2ee997
Pink_Cats <1907284584@qq.com>
2025-10-05 09:58:19 +0800

    Fix tack problem.

--- src/main/java/com/Pink_Cats/createschematicchecker/core/BlueEngine/NbtFunc.java
+++ src/main/java/com/Pink_Cats/createschematicchecker/core/BlueEngine/NbtFunc.java
@@ -7,6 +7,7 @@ import java.util.*;
 
 import static com.Pink_Cats.createschematicchecker.FancyConfig.ConfigRegister.*;
 import static com.Pink_Cats.createschematicchecker.core.BlueEngine.BlockSweeper.ClearBanBlock;
+import static com.Pink_Cats.createschematicchecker.core.BlueEngine.TagFunc.PaletteGetId;
 import static com.Pink_Cats.createschematicchecker.core.ChainEngine.ConveyorInterface.StringPickHalfPos;
 import static com.Pink_Cats.createschematicchecker.core.ChainEngine.ConveyorInterface.StringPickPos;
 import static com.Pink_Cats.createschematicchecker.core.BlueEngine.NbtInterFace.*;
@@ -641,6 +642,24 @@ public class NbtFunc {
             }
         }
         if (type.equals("palette")) {
+            String id = PaletteGetId(data);
+            if (id.equals("create:track")){
+                CompoundTag Properties = data.getCompound("Properties");
+                String track_shape = NoQuotes(data.getCompound("Properties")
+                        .get("shape").toString());
+
+                if (track_shape.equals("none") || track_shape.equals("NONE")) {
+                    Properties.put("shape",TagString("xo"));
+                    Message.FE(translateDirect("csc.trams.track_problem"));
+
+                }
+
+
+            }
+
+
+
+
             if (HasBanBlock) {
                 MapData = ClearBanBlock(data,"palette",sequence,PaletteBlockData);
                 data = S_tag(MapData.get("Data"));
