56b1648ce3bd6b579025db21dfe288f020bd437a
Pink_Cats <1907284584@qq.com>
2025-10-14 17:31:33 +0800

    1. Remove junk Output. 2. Add a new function. Now you can use /csc help to know what's new. 3. Fix a bug when checking copycats.

--- src/main/java/com/Pink_Cats/createschematicchecker/echo/Commands.java
+++ src/main/java/com/Pink_Cats/createschematicchecker/echo/Commands.java
@@ -1,6 +1,7 @@
 package com.Pink_Cats.createschematicchecker.echo;
 
 import com.Pink_Cats.createschematicchecker.FancyConfig.ConfigRegister;
+import com.Pink_Cats.createschematicchecker.event.TempOffEvent;
 import com.Pink_Cats.createschematicchecker.lang.Message;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
@@ -17,6 +18,8 @@ import java.util.List;
 import static com.Pink_Cats.createschematicchecker.FancyConfig.ConfigRegister.*;
 import static com.Pink_Cats.createschematicchecker.FancyConfig.ConfigRegister.ID_modify_rule_manual;
 import static com.Pink_Cats.createschematicchecker.FancyConfig.ConfigRegister.ID_modify_rule_online;
+import static com.Pink_Cats.createschematicchecker.core.BlueEngine.NbtInterFace.S_bool;
+import static com.Pink_Cats.createschematicchecker.event.TempOffEvent.Temporary_stop;
 import static com.Pink_Cats.createschematicchecker.lang.CSCLanguage.translateDirect;
 import static com.Pink_Cats.createschematicchecker.network.SimpleJsonParser.merge;
 
@@ -87,7 +90,24 @@ public class Commands {
                                             return Command.SINGLE_SUCCESS;
                                         }
                                 ))
-
+                        .then(net.minecraft.commands.Commands.literal("DisableTemp")
+                                .executes(context ->  {
+                                            Player player = context.getSource().getPlayer();
+                                            if (player != null) {
+                                                CSC_DISABLE(player);
+                                            }
+                                            return Command.SINGLE_SUCCESS;
+                                        }
+                                ))
+                        .then(net.minecraft.commands.Commands.literal("Enable")
+                                .executes(context ->  {
+                                            Player player = context.getSource().getPlayer();
+                                            if (player != null) {
+                                                CSC_ENABLE(player);
+                                            }
+                                            return Command.SINGLE_SUCCESS;
+                                        }
+                                ))
                         .then(net.minecraft.commands.Commands.literal("list")
                                 .executes(context -> {
                                     Player player = context.getSource().getPlayer();
@@ -213,6 +233,26 @@ public class Commands {
     }
 
 
+    public static void CSC_DISABLE(Player player) {
+
+        player.sendSystemMessage(Component.literal(translateDirect("console.disable.csc"))
+                .setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)));
+
+        Temporary_stop = true;
+
+    }
+
+
+    public static void CSC_ENABLE(Player player) {
+
+        player.sendSystemMessage(Component.literal(translateDirect("csc.off.temporary.restore"))
+                .setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)));
+
+        TempOffEvent.StopTick  = 5;
+
+    }
+
+
     public static void CSC_HELP(Player player){
         player.sendSystemMessage(Component.literal(translateDirect("console.csc.run.command"))
                 .setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)));
@@ -236,6 +276,13 @@ public class Commands {
                 .setStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)));
         player.sendSystemMessage(Component.literal(translateDirect("console.csc.welcome5"))
                 .setStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)));
+
+        player.sendSystemMessage(Component.literal(translateDirect("console.csc.welcome6"))
+                .setStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)));
+        player.sendSystemMessage(Component.literal(translateDirect("console.csc.welcome7"))
+                .setStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)));
+
+
         player.sendSystemMessage(Component.literal(translateDirect("console.csc.liner"))
                 .setStyle(Style.EMPTY.withColor(ChatFormatting.GOLD)));
     }
---
+++ update/1.20.1-0.21.6-0.5.1j.md
@@ -0,0 +1,3 @@
+1. Remove junk Output.
+2. Add a new function. Now you can use /csc help to know what's new.
+3. Fix a bug when checking copycats.
